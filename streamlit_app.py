88affeeb7daee4d4c70c27ebf7e5487627928ec7
Mei02-11 <haimei0211@gmail.com>
2025-11-13 15:13:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("🎈 testtt")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.header("Hello world!") 
+st.subheader("Hello world!!")
+st.text("Hello world!!!")
+# Create a dropdown menu for selecting a hobby
+hobby = st.selectbox("Select a Hobby:", ['Dancing', 'Reading', 'Sports'])
+
+# Display the selected hobby
+st.write("Your hobby is:", hobby)
